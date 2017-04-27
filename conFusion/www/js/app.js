631eec6a2f15d6492223e64a8f2ce70492f92e8c
eomsanghyun <pnukk12@gmail.com>
2017-04-27 18:10:32 +0900

    learn local storage

--- conFusion/www/js/app.js
+++ conFusion/www/js/app.js
@@ -13,7 +13,6 @@ angular.module('conFusion', ['ionic', 'conFusion.controllers','conFusion.service
     if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
-
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
